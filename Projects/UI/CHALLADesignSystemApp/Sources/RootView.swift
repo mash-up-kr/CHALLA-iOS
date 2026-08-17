01d03b117b3614a51435dcec230a6cabe85f4a99
Hansangjin98 <onetable98@naver.com>
2026-08-18 04:29:59 +0900

    [Feat] #61 - Tooltip 검수 갤러리 추가 (position×arrowAlignment 12조합)

--- Projects/UI/CHALLADesignSystemApp/Sources/RootView.swift
+++ Projects/UI/CHALLADesignSystemApp/Sources/RootView.swift
@@ -59,6 +59,9 @@ struct RootView: View {
                     NavigationLink("Toast") {
                         ToastGallery()
                     }
+                    NavigationLink("Tooltip") {
+                        TooltipGallery()
+                    }
                 }
 
                 // 아직 토큰이 아닌 것들의 체험·제안 공간. 디자이너 선택이 확정되면 토큰으로 승격한다.
